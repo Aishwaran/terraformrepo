35c674be1781ac17e601b32779747938cc3394be
Aishwaran <168258440+Aishwaran@users.noreply.github.com>
2024-08-29 19:18:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,4 +11,8 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-
+resource "aws_instance" "web" {
+  ami = "ami-0c2af51e265bd5e0e" # Amazon Linux
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "web-2"
